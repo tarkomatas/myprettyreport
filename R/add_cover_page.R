87958d28eea85ff9f09a445ae613f5c46424ffdc
Tamas Marko <marko.tamas1991@gmail.com>
2018-11-10 11:06:02 +0100

    extra_layout_params paramter added

--- R/add_cover_page.R
+++ R/add_cover_page.R
@@ -22,6 +22,16 @@
 #' @param theme Character string which specifies the theme of the report.
 #' Possible values are "basic" and "flashy". Try both
 #' themes and check the differences.
+#' @param extra_layout_params User defined grid functions can be
+#' passed in the PDF report to modify the layout region.
+#' It has to be defined as a dummy function, see the example section
+#' for more details.
+#' @param title Character string which allows to set up user defined title
+#' to a cover page. The default value is the previously defined
+#' start_report() title parameter.
+#' @param subtitle Character string which allows to set up user defined subtitle
+#' to a cover page. The default value is the previously defined
+#' start_report() subtitle parameter.
 #' @examples
 #' # minimal example
 #' # it generates PDF file with only a cover page.
@@ -37,7 +47,7 @@
 #' # add a logo to a report
 #' # 1. load the logo file.
 #' library(magick)
-#' sample_logo <- image_read("http://myhappydata.com/img/1.png")
+#' sample_logo <- magick::image_read("http://myhappydata.com/img/1.png")
 #' # 2. add the loaded file to a report.
 #' start_report() %>%
 #'   add_cover_page(logo = sample_logo) %>%
@@ -51,20 +61,41 @@
 #'   ) %>%
 #'   end_report()
 #'
+#' # pass extra grid paramters into the report
+#' cp_e_params = function() {
+#'    grid::grid.rect(gp = grid::gpar(fill = "grey"),
+#'                    vp = grid::viewport(layout.pos.row = 2,
+#'                                        layout.pos.col = 1),
+#'                    width = 0.8, height = 0.2)
+#'    grid::grid.text("example text",
+#'                    vp = grid::viewport(layout.pos.row = 2,
+#'                                        layout.pos.col = 1))
+#' }
+#'
+#' start_report() %>%
+#'   add_cover_page(
+#'     extra_layout_params = cp_e_params
+#'   ) %>%
+#'   end_report()
 #' @export
 
 add_cover_page <- function(..., header_color = "#f44242",
                            footer_color = "#3d3c3c", creaton_time = Sys.Date(),
                            logo = NA, logo_size = NULL,
-                           theme = c("basic", "flashy")) {
+                           theme = c("basic", "flashy"),
+                           extra_layout_params = function(){},
+                           title = myprettyreport.env$report_title,
+                           subtitle = myprettyreport.env$report_subtitle) {
   check_if_start_report_function_has_been_called(list(...))
   theme <- match.arg(theme)
   grid::grid.newpage()
   if (theme == "basic") {
     set_basic_cover_theme(header_color, footer_color, creaton_time,
-                          logo, logo_size)
+                          logo, logo_size, title, subtitle)
   } else if (theme == "flashy") {
     set_flashy_cover_theme(header_color, footer_color, creaton_time,
-                           logo, logo_size)
+                           logo, logo_size, title, subtitle)
   }
+  extra_layout_params()
+  myprettyreport.env$add_cover_page_executed <- TRUE
 }
